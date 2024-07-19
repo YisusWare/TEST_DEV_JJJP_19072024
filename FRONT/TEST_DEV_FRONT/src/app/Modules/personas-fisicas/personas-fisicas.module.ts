8f9847f422fc01c196a982abe2ef6146b199bda9
Jesús Jáuregui <155684107+YisusWare27@users.noreply.github.com>
2024-07-19 13:27:03 -0700

    login created

---
+++ FRONT/TEST_DEV_FRONT/src/app/Modules/personas-fisicas/personas-fisicas.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PersonasFisicasRoutingModule } from './personas-fisicas-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PersonasFisicasRoutingModule
+  ]
+})
+export class PersonasFisicasModule { }
