b44412ad3664c731e518ed9d70df7588d5881d9d
Jesús Jáuregui <155684107+YisusWare27@users.noreply.github.com>
2024-07-21 09:16:03 -0600

    logout button fixed

--- FRONT/TEST_DEV_FRONT/src/app/Modules/components/navbar/navbar.component.ts
+++ FRONT/TEST_DEV_FRONT/src/app/Modules/components/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { MenuItem } from 'primeng/api';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -8,7 +10,8 @@ import { MenuItem } from 'primeng/api';
 })
 export class NavbarComponent implements OnInit {
 
-
+  constructor(private authService:AuthService, private router: Router){}
+  userEmail: string = '';
   ngOnInit(): void {
     
     this.items = [
@@ -17,15 +20,29 @@ export class NavbarComponent implements OnInit {
           icon: 'pi pi-home',
           routerLink: 'personas-fisicas'
       }
+
+      
       // ,
       // {
       //   label: 'Reportes',
       //   icon: 'pi pi-home',
       //   routerLink: 'reportes'
       // }
-    ]
+    ];
+
+    const user = localStorage.getItem('user');
+
+    if(user){
+      this.userEmail = JSON.parse(user).email;
+    }
   }
 
+
   items: MenuItem[] | undefined;
+
+  logout(){
+    this.authService.logout();
+    this.router.navigateByUrl('/');
+  }
 
 }
